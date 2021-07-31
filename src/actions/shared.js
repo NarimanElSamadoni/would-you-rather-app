45d1013ded4dc642a99484d170063fe1bdf046a4
NarimanElSamadoni <nariman.elsamadoni@gmail.com>
2021-08-01 00:46:16 +0200

    Add new packages, initialize redux store, and create actions, reducers, and middleware
    Install redux, react-redux, and redux-thunk packages
    Create actions, reducers, and middleware
    Initialize store with reducers and middleware created

---
+++ src/actions/shared.js
@@ -0,0 +1,19 @@
+import { getInitialData } from '../utils/api'
+import { setAuthedUser } from './authedUser'
+import { getQuestions } from './questions'
+import { getUsers } from './users'
+
+const AUTHED_ID = 'tylermcginnis'
+
+export function handleInitialData () {
+  return (dispatch) => {
+    // dispatch(showLoading())
+    return getInitialData()
+      .then(({ users, questions }) => {
+        dispatch(getUsers(users))
+        dispatch(getQuestions(questions))
+        dispatch(setAuthedUser(AUTHED_ID))
+        // dispatch(hideLoading())
+      })
+  }
+}
